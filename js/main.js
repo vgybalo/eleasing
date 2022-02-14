806e7bbfd4f52db74372160284ff06fdc7221928
VladimirHV <vgybalo@ukr.net>
2022-02-15 00:53:01 +0200

    push2

--- js/main.js
+++ js/main.js
@@ -62,75 +62,14 @@ faqQuestionSmallClose3.addEventListener('click', ()=> {
     faqQuestionSmallPlus3.style.display='block';
     });
 
+
+
+    /***aplication form*/
+    // const aplicationFormShort = document.forms['aplication-form-short'];
+    // aplicationFormShort.addEventListener('submit', (ev) => {
+    //     ev.preventDefault();
+    //     const apFormShortFormdata = new FormData(ev.target);
+    //     axios.post('/clients',apFormShortFormdata);
+        
     
-    /*partner-comments-slider*/
-/*const partnerCommentsSliderLeftarrow = document.getElementById("partner-comments-slider-leftarrow");
-const partnerCommentsSliderRightarrow = document.getElementById("partner-comments-slider-rightarrow");
-const partnerCommentsSliderMainscreenSlide = document.getElementById("partner-comments-slider-mainscreen-slide");
-const partnerCommentsSliderMainscreen = document.getElementById("partner-comments-slider-mainscreen");
-let i=1;
-let j=1;
-partnerCommentsSliderLeftarrow.addEventListener('click', ()=> {
-partnerCommentsSliderMainscreen.style.left='-225px'*/
-
-   //partnerCommentsSliderMainscreenSlide.style.transform='translateX(-230px)'
-    /* let translateSize=-230*i;
-    partnerCommentsSliderMainscreenSlide.style.transform=`translateX(-${translateSize}px)`;
-    i++*/
-   /* });
-
-partnerCommentsSliderRightarrow.addEventListener('click', ()=> {
-    partnerCommentsSliderMainscreen.style.left='225px'*/
-    /*let translateSizeRight=230*j;
-    partnerCommentsSliderMainscreenSlide.style.transform=`translateX(-${translateSizeRight}px)`;
-    j++*/
-    /*});*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // /* этот код помечает картинки, для удобства разработки */
-    // let i = 1;
-    // for(let li of carousel.querySelectorAll('li')) {
-    //   li.style.position = 'relative';
-    //   li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-    //   i++;
-    // }
-
-    // /* конфигурация */
-    // let width = 200; // ширина картинки
-    // let count = 3; // видимое количество изображений
-
-    // let list = carousel.querySelector('ul');
-    // let listElems = carousel.querySelectorAll('li');
-
-    // let position = 1; // положение ленты прокрутки
-
-    // carousel.querySelector('.prev').onclick = function() {
-    //   // сдвиг влево
-    //   position += width * 1;
-    //   // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
-    //   position = Math.min(position, 0)
-    //   list.style.marginLeft = position + 'px';
-    // };
-
-    // carousel.querySelector('.next').onclick = function() {
-    //   // сдвиг вправо
-    //   position -= width * 1;
-    //   // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-    //   position = Math.max(position, -width * (listElems.length - count));
-    //   list.style.marginLeft = position + 'px';
-    // };
+    // })
